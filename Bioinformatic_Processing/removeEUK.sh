538611e72ff02f06682cf18e2f26d4ad54cc924a
Jacob Agerbo Rasmussen <Genomicsisawesome@gmail.com>
2023-11-24 15:04:21 +0100

    adding threads

--- Bioinformatic_Processing/removeEUK.sh
+++ Bioinformatic_Processing/removeEUK.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-remove_EUK() {
+
+run_analysis() {
     # Load necessary modules
     module load trimmomatic kraken2/2.1.2 pigz/2.6.0
 
@@ -20,6 +21,10 @@ remove_EUK() {
                 ;;
             --kraken-db)
                 kraken_db="$2"
+                shift 2
+                ;;
+            --threads)
+                threads="$2"
                 shift 2
                 ;;
             *)
@@ -50,18 +55,23 @@ remove_EUK() {
     output_file_2=$OUT/${sample_name}_filtered_2.fastq.gz
 
     # Run Trimmomatic to trim low quality reads
-    trimmomatic PE -threads $SLURM_CPUS_PER_TASK -phred33 $input_file_1 $input_file_2 $output_file_1 /dev/null $output_file_2 /dev/null LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:50
+    trimmomatic PE -threads $threads -phred33 $input_file_1 $input_file_2 $output_file_1 /dev/null $output_file_2 /dev/null LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:50
 
     # Remove eukaryotic data from the metagenome using Kraken2
     DATA="$OUT"
-    
-    kraken2 --db $kraken_db --threads $SLURM_CPUS_PER_TASK --gzip-compressed --paired $output_file_1 $output_file_2 --output "$sample_name"_12_krk2_500.kraken2 > "$sample_name"_12_krk2_500.kraken2.report
+    /projects/mjolnir1/apps/conda/kraken2-2.1.2/bin/kraken2 --db $kraken_db --threads $threads --gzip-compressed --paired $output_file_1 $output_file_2 > $DATA/${sample_name}_kraken_output.txt
 
-    pigz -p $SLURM_CPUS_PER_TASK $OUT/"$sample_name"_12_krk2_500.kraken2 $OUT/"$sample_name"_12_krk2_500.kraken2.report
+    # Filter out eukaryotic reads from the metagenome using Kraken2 output
+    /projects/mjolnir1/apps/bin/extract_kraken_readsBB.py -k $OUT/"$sample_name"_12_krk2_500.kraken2.gz \
+                                        -s1 $DATA/"$sample_name"_filtered_1.fastq.gz \
+                                        -s2 $DATA/"$sample_name"_filtered_2.fastq.gz \
+                                        -o $OUT/noEUK_"$sample_name"_1.fq \
+                                        -o2 $OUT/noEUK_"$sample_name"_2.fq \
+                                        --fastq-output  --exclude --taxid 2759 \
+                                        --include-children  \
+                                        -r $OUT/"$sample_name"_12_krk2_500.kraken2.report.gz
 
-    /projects/mjolnir1/apps/bin/extract_kraken_readsBB.py -k $OUT/"$sample_name"_12_krk2_500.kraken2.gz -s1 $DATA/"$sample_name"_filtered_1.fastq.gz -s2 $DATA/"$sample_name"_filtered_2.fastq.gz -o $OUT/noEUK_"$sample_name"_1.fq -o2 $OUT/noEUK_"$sample_name"_2.fq --fastq-output  --exclude --taxid 2759 --include-children  -r $OUT/"$sample_name"_12_krk2_500.kraken2.report.gz
-
-    pigz -p $SLURM_CPUS_PER_TASK $OUT/noEUK_"$sample_name"_1.fq $OUT/noEUK_"$sample_name"_2.fq
+    pigz -p $threads $OUT/noEUK_"$sample_name"_1.fq $OUT/noEUK_"$sample_name"_2.fq
 
     # Check if noEUK files exist before removing filtered files
     if [[ -e $OUT/noEUK_"$sample_name"_1.fq.gz && -e $OUT/noEUK_"$sample_name"_2.fq.gz ]]; then
@@ -76,6 +86,5 @@ remove_EUK() {
         exit 1
     fi
 }
-
 
 #bash removeEUK.sh --input /data/samples/ --output /data/output/ --sample sample1 --kraken-db /projects/mjolnir1/apps/databases/kraken2/minikraken2_v2_8GB --threads 4

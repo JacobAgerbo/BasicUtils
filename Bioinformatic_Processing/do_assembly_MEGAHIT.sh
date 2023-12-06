94dda8cd035413d97461b704246bc2f5913cff94
Jacob Agerbo Rasmussen <Genomicsisawesome@gmail.com>
2023-12-06 07:59:03 +0100

    .

--- Bioinformatic_Processing/do_assembly_MEGAHIT.sh
+++ Bioinformatic_Processing/do_assembly_MEGAHIT.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+module load miniconda megahit
 conda activate vaplinev2
-module load spades
 # Default parameter values
 DATA=''
 OUT=''
 
 # Parse command line arguments
-while getopts "d:o:" opt; do
+while getopts "d:o:s:" opt; do
   case ${opt} in
     d )
       DATA=${OPTARG}
       ;;
     o )
       OUT=${OPTARG}
+      ;;
+    s )
+      SAMPLE=${OPTARG}
       ;;
     \? )
       echo "Invalid option: -$OPTARG" >&2
@@ -26,18 +29,19 @@ while getopts "d:o:" opt; do
 done
 
 # Check if required parameters are provided
-if [ -z "$DATA" ] || [ -z "$OUT" ]; then
-  echo "Usage: bash do_assembly.sh -d /path/to/input/files -o /path/to/output/files"
+if [ -z "$DATA" ] || [ -z "$OUT" ] || [ -z "$SAMPLE" ]; then
+  echo "Usage: bash do_MEGAHIT.sh -d /path/to/input/files -o /path/to/output/files -s sample" 
   exit 1
 fi
 
+
 # Replace <file1> and <file2> with the names of your paired-end read files
-file1=noEUK_"$1"_1.fq
-file2=noEUK_"$1"_2.fq
+file1=noEUK_"$SAMPLE"_1.fq
+file2=noEUK_"$SAMPLE"_2.fq
 
 # Count the number of reads in each file
-count1=$(grep -c "^@" $DATA/$file1.gz)
-count2=$(grep -c "^@" $DATA/$file2.gz)
+count1=$(zcat $DATA/$file1.gz | echo $((`wc -l`/4)))
+count2=$(zcat $DATA/$file2.gz | echo $((`wc -l`/4)))
 echo "$file1 contained $count1 reads"
 echo "$file2 contained $count2 reads"
 
@@ -53,13 +57,13 @@ if [ $count1 -ne $count2 ]; then
             UNICORN='\U1F984'; echo -e "$(tput setaf 1)Files are ready for fastq_pair ${UNICORN}$(tput sgr 0)"
         fi
     #
-        if [[ ! -f $DATA/fixed_"$1"_1.fq.gz && ! -f $DATA/fixed_"$1"_2.fq.gz ]]; then
+        if [[ ! -f $DATA/fixed_"$SAMPLE"_1.fq.gz && ! -f $DATA/fixed_"$SAMPLE"_2.fq.gz ]]; then
             THUMBS_UP='\U1F44D'; echo -e "$(tput setaf 1)paired files dont exist. Starting re-pairing. $THUMBS_UP $(tput sgr 0)"
             fastq_pair $DATA/$file1 $DATA/$file2
-            mv $DATA/$file1.paired.fq $DATA/fixed_"$1"_1.fq
-            mv $DATA/$file2.paired.fq $DATA/fixed_"$1"_2.fq
-            pigz $DATA/fixed_"$1"_1.fq -p $SLURM_CPUS_PER_TASK
-            pigz $DATA/fixed_"$1"_2.fq -p $SLURM_CPUS_PER_TASK
+            mv $DATA/$file1.paired.fq $DATA/fixed_"$SAMPLE"_1.fq
+            mv $DATA/$file2.paired.fq $DATA/fixed_"$SAMPLE"_2.fq
+            pigz $DATA/fixed_"$SAMPLE"_1.fq -p $SLURM_CPUS_PER_TASK
+            pigz $DATA/fixed_"$SAMPLE"_2.fq -p $SLURM_CPUS_PER_TASK
             pigz $DATA/$file1 -p $SLURM_CPUS_PER_TASK
             pigz $DATA/$file2 -p $SLURM_CPUS_PER_TASK
          else
@@ -67,21 +71,24 @@ if [ $count1 -ne $count2 ]; then
         fi
     
     echo "Now I will continue an assembly with fixed reads"
-    module load spades
-    metaspades.py -o $OUT/$1 \
-                    -1 $DATA/fixed_$1_1.fq.gz \
-                    -2 $DATA/fixed_$1_2.fq.gz \
-                    -k 27,53,119,127 \
-                    --threads $SLURM_CPUS_PER_TASK \
-                    -m 384
+    
+    megahit -1 $DATA/fixed_$SAMPLE_1.fq.gz \
+            -2 $DATA/fixed_$SAMPLE_2.fq.gz \
+        --min-contig-len 1000 \
+        -t $SLURM_CPUS_PER_TASK \
+        --presets meta-sensitive \
+        -o $OUT/$SAMPLE
+
 else
     echo "The paired-end read files already have an equal number of reads."
     echo "Now I will continue an assembly with original reads"
-    module load spades
-    metaspades.py -o $OUT/$1 \
-                    -1 $DATA/$file1.gz \
-                    -2 $DATA/$file2.gz \
-                    -k 27,53,119,127 \
-                    --threads $SLURM_CPUS_PER_TASK \
-                    -m 384
+    module load megahit
+    megahit -1 $DATA/$file1.gz \
+        -2 $DATA/$file2.gz \
+        --min-contig-len 1000 \
+        -t $SLURM_CPUS_PER_TASK \
+        --presets meta-sensitive \
+        -o $OUT/$SAMPLE
+
+
 fi
